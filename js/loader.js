ac67baf1474b8efd890749325806039cf0cb8d65
DANIEL SANMIGUEL <danielsanmiguel802@gmail.com>
2026-04-22 21:32:51 -0500

    navbar

---
+++ js/loader.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    fetch("components/navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar").innerHTML = data;
+        });
+
+    fetch("components/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
+
+});
